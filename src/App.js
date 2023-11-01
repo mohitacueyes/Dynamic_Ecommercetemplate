f60e64c648d25d55335786ee446145a310e5a41a
hirparamayank <mayank.acueyes@gmail.com>
2023-11-01 16:01:12 +0530

    About

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { Footer } from './Components/Layout/Footer/Footer';
+import { About } from './Components/Pages/About';
+import Contact from './Components/Pages/Contact/Contact';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/"  element={<h1>Home</h1>}></Route>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
       </Routes>
+      <Footer />
     </>
   );
 }
