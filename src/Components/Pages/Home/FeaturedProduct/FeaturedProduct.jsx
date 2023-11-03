abcdce37de0984ccec20a3f18000928402e9bf5f
hirparamayank <mayank.acueyes@gmail.com>
2023-11-03 15:05:34 +0530

    subcategorydata

--- src/Components/Pages/Home/FeaturedProduct/FeaturedProduct.jsx
+++ src/Components/Pages/Home/FeaturedProduct/FeaturedProduct.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 const FeaturedProduct = () => {
   const [featuredProducts, setFeaturedProducts] = useState([]);
+  // -------ADD TO CART --------//
 
   useEffect(() => {
     // Fetch data from your API
@@ -15,82 +16,84 @@ const FeaturedProduct = () => {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
   console.log(featuredProducts);
+
+  
   return (
     <>
       {/*start Featured product*/}
       <section className="py-4">
-        <div className="container">
-          <div className="separator pb-4">
-            <div className="line" />
-            <h5 className="mb-0 fw-bold separator-title">FEATURED PRODUCTS</h5>
-            <div className="line" />
-          </div>
-          <div className="product-grid">
-          {featuredProducts && featuredProducts.map((feed) => (
-            <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-5 g-3 g-sm-4">
-              {feed.homefeed_product.map((product) => (
-              <div className="col">
-                <div className="card">
-                  <div className="position-relative overflow-hidden">
-                    <div className="add-cart position-absolute top-0 end-0 mt-3 me-3">
-                      <a href="javascript:;">
-                        <i className="bx bx-cart-add" />
-                      </a>
-                    </div>
-                    <div className="quick-view position-absolute start-0 bottom-0 end-0">
-
-                      <Link
-                       to={`/productdetails/${product.id}`}                    
-                      >
-                        Quick View
-                      </Link>
-                    </div>
-                    <a href="/productdetails">
-                      <img
-                        src="assets/images/products/01.png"
-                        className="img-fluid"
-                        alt="..."
-                      />
-                    </a>
-                  </div>
-                  <div className="card-body px-0">
-                    <div className="d-flex align-items-center justify-content-between">
-                      <div className>
-                        <p className="mb-1 product-short-name"></p>
-                        <h6 className="mb-0 fw-bold product-short-title">
-                         {product.name}
-                        </h6>
+        {featuredProducts &&
+          featuredProducts.map((feed) => (
+            <div className="container">
+              <div className="separator pb-4">
+                <div className="line" />
+                <h5 className="mb-0 fw-bold separator-title">
+                  {feed.feedname}
+                </h5>
+                <div className="line" />
+              </div>
+              <div className="product-grid">
+                <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-5 g-3 g-sm-4">
+                  {feed.homefeed_product.map((product) => (
+                    <div className="col">
+                      <div className="card">
+                        <div className="position-relative overflow-hidden">
+                          <div className="add-cart position-absolute top-0 end-0 mt-3 me-3">
+                            <a href="javascript:;">
+                              <i className="bx bx-cart-add" />
+                            </a>
+                          </div>
+                          <div className="quick-view position-absolute start-0 bottom-0 end-0">
+                            <Link to={`/productdetails/${product.id}`}>
+                              Quick View
+                            </Link>
+                          </div>
+                          <a>
+                            <img
+                              src="assets/images/products/01.png"
+                              className="img-fluid"
+                              alt="..."
+                            />
+                          </a>
+                        </div>
+                        <div className="card-body px-0">
+                          <div className="d-flex align-items-center justify-content-between">
+                            <div className>
+                              <p className="mb-1 product-short-name"></p>
+                              <h6 className="mb-0 fw-bold product-short-title">
+                                {product.name}
+                              </h6>
+                            </div>
+                            <div className="icon-wishlist">
+                              <a href="javascript:;">
+                                <i className="bx bx-heart" />
+                              </a>
+                            </div>
+                          </div>
+                          <div className="cursor-pointer rating mt-2">
+                            <i className="bx bxs-star text-warning" />
+                            <i className="bx bxs-star text-warning" />
+                            <i className="bx bxs-star text-warning" />
+                            <i className="bx bxs-star text-warning" />
+                            <i className="bx bxs-star text-warning" />
+                          </div>
+                          <div className="product-price d-flex align-items-center justify-content-start gap-2 mt-2">
+                            <div className="h6 fw-light fw-bold text-secondary text-decoration-line-through">
+                              ${product.price}
+                            </div>
+                            <div className="h6 fw-bold">
+                              ${product.discounted_price}
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                      <div className="icon-wishlist">
-                        <a href="javascript:;">
-                          <i className="bx bx-heart" />
-                        </a>
-                      </div>
                     </div>
-                    <div className="cursor-pointer rating mt-2">
-                      <i className="bx bxs-star text-warning" />
-                      <i className="bx bxs-star text-warning" />
-                      <i className="bx bxs-star text-warning" />
-                      <i className="bx bxs-star text-warning" />
-                      <i className="bx bxs-star text-warning" />
-                    </div>
-                    <div className="product-price d-flex align-items-center justify-content-start gap-2 mt-2">
-                      <div className="h6 fw-light fw-bold text-secondary text-decoration-line-through">
-                        ${product.price}
-                      </div>
-                      <div className="h6 fw-bold">${product.discounted_price}</div>
-                    </div>
-                  </div>
+                  ))}
                 </div>
               </div>
-                ))}
             </div>
-            ))}
-            {/*end row*/}
-          </div>
-        </div>
+          ))}
       </section>
-      {/*end Featured product*/}
     </>
   );
 };
